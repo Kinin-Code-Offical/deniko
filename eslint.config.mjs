7508f422f61c8211ff428505c66e3e55365a9b4b
Kinin-Code-Offical <125186556+Kinin-Code-Offical@users.noreply.github.com>
2025-12-03 21:17:45 +0300

    refactor: update ESLint and TypeScript configurations to include pre-codes directory in ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,9 @@ const eslintConfig = defineConfig([
   ...nextTs,
   // Override default ignores of eslint-config-next.
   globalIgnores([
+    //backup files created by me like an idiot
+    "pre-codes/**",
+
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
