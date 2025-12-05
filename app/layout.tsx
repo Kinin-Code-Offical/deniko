d1644f445617c25067f84370eac970f082b0f876
Kinin-Code-Offical <125186556+Kinin-Code-Offical@users.noreply.github.com>
2025-12-06 01:21:42 +0300

    Add new icon image (icon1.png) to the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,15 +44,10 @@ export const metadata: Metadata = {
     locale: "tr_TR",
     type: "website",
   },
-  icons: {
-    icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    ],
-    apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
-    ],
+  appleWebApp: {
+    title: "Deniko",
+    statusBarStyle: "default",
+    capable: true,
   },
   manifest: "/site.webmanifest",
 };
