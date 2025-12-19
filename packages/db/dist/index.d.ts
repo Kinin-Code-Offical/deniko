3579bbb835112f6320c86fdbd7a9b352d5bd52e1
Kinin-Code-Offical <125186556+Kinin-Code-Offical@users.noreply.github.com>
2025-12-19 18:28:59 +0300

    fix: update import path for PrismaClient options in TypeScript definitions
    chore: add .npmrc to configure public hoisting for Prisma packages

--- packages/db/dist/index.d.ts
+++ packages/db/dist/index.d.ts
@@ -29,4 +29,4 @@ export * from '@prisma/client';
 export declare const createDb: (config: {
     isProduction: boolean;
     datasourceUrl?: string;
-}) => PrismaClient<import("@prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/client").DefaultArgs>;
+}) => PrismaClient<import("@prisma/client").Prisma.PrismaClientOptions, never, import(".prisma/client/runtime/client").DefaultArgs>;
